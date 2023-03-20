c9b7981361bc2b87bd09e8e08b9d4223c236a3c0
tektechnologies <info@tektechnologies.com>
2023-03-20 11:34:29 -0500

    add images and hidden files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 'use strict';
+console.log('js connected');
+
+
+
+// Problem Domain: the Cedar Rapids Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
+
+//Each Kitten's profile should have:
+// Name
+// Breed
+// - gender   -- isFixed.
+// - random age assigned
+// a list of what they like. interests
+// an image
+// is good with kids
+// is good with dogs
+// is good with other animals
